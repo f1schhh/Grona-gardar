9c4ec67cfa8e23473bdf8b7b3d052c20c350f6bb
Adam Fischer <38475224+f1schhh@users.noreply.github.com>
2024-02-14 14:42:32 +0100

    Uppdatering

--- src/views/ProductsPageview.vue
+++ src/views/ProductsPageview.vue
@@ -1,282 +1,316 @@
 <script></script>
 
 <template>
-    <article>
-        <section>
-            <h2>Frukt</h2>
-            <ul>
-                <li>
-                    <div class="specific_product">
-                        <div class="img-wrapper">
-                            <img src="../assets/media/Alice_Apple.jpg" alt="">
-                        </div>
-                        <div class="h3_and_heart">
-                            <h3>Äpple Alice</h3>
-                            <div class="button_like">
-                                <i class="bi bi-heart"></i>
-                            </div>
-                        </div>
-
-                        <button class="button_cart">Lägg i varukorg</button>
-
-
-                    </div>
-                </li>
-                <li>
-                    <div class="specific_product">
-                        <div class="img-wrapper">
-                            <img src="../assets/media/Alice_Apple.jpg" alt="">
-                        </div>
-                        <div class="h3_and_heart">
-                            <h3>Äpple Alice</h3>
-                            <div class="button_like">
-                                <i class="bi bi-heart"></i>
-                            </div>
-                        </div>
-
-                        <button class="button_cart">Lägg i varukorg</button>
-
-
-                    </div>
-                </li>
-                <li>
-                    <div class="specific_product">
-                        <div class="img-wrapper">
-                            <img src="../assets/media/Alice_Apple.jpg" alt="">
-                        </div>
-                        <div class="h3_and_heart">
-                            <h3>Äpple Alice</h3>
-                            <div class="button_like">
-                                <i class="bi bi-heart"></i>
-                            </div>
-                        </div>
-
-                        <button class="button_cart">Lägg i varukorg</button>
-
-
-                    </div>
-                </li>
-                <li>
-                    <div class="specific_product">
-                        <div class="img-wrapper">
-                            <img src="../assets/media/Alice_Apple.jpg" alt="">
-                        </div>
-                        <div class="h3_and_heart">
-                            <h3>Äpple Alice</h3>
-                            <div class="button_like">
-                                <i class="bi bi-heart"></i>
-                            </div>
-                        </div>
-
-                        <button class="button_cart">Lägg i varukorg</button>
-                    </div>
-                </li>
-            </ul>
-            <div class="div_button_more_products">
-                <button class="button_more_products">Mer Frukter <i class="bi bi-arrow-right"></i></button>
+  <article>
+    <section>
+      <h2>Frukt</h2>
+      <ul>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Alice_Apple.jpg" alt="">
             </div>
-        </section>
-
-        <section>
-            <h2>Grönsaker</h2>
-            <ul>
-                <li>
-                    <div class="specific_product">
-                        <div class="img-wrapper">
-                            <img src="../assets/media/Carrot.jpg" alt="">
-                        </div>
-                        <div class="h3_and_heart">
-                            <h3>Äpple Alice</h3>
-                            <div class="button_like">
-                                <i class="bi bi-heart"></i>
-                            </div>
-                        </div>
-
-                        <button class="button_cart">Lägg i varukorg</button>
-
-
-                    </div>
-                </li>
-                <li>
-                    <div class="specific_product">
-                        <div class="img-wrapper">
-                            <img src="../assets/media/Carrot.jpg" alt="">
-                        </div>
-                        <div class="h3_and_heart">
-                            <h3>Äpple Alice</h3>
-                            <div class="button_like">
-                                <i class="bi bi-heart"></i>
-                            </div>
-                        </div>
-
-                        <button class="button_cart">Lägg i varukorg</button>
-
-
-                    </div>
-                </li>
-                <li>
-                    <div class="specific_product">
-                        <div class="img-wrapper">
-                            <img src="../assets/media/Carrot.jpg" alt="">
-                        </div>
-                        <div class="h3_and_heart">
-                            <h3>Äpple Alice</h3>
-                            <div class="button_like">
-                                <i class="bi bi-heart"></i>
-                            </div>
-                        </div>
-
-                        <button class="button_cart">Lägg i varukorg</button>
-
-
-                    </div>
-                </li>
-                <li>
-                    <div class="specific_product">
-                        <div class="img-wrapper">
-                            <img src="../assets/media/Carrot.jpg" alt="">
-                        </div>
-                        <div class="h3_and_heart">
-                            <h3>Äpple Alice</h3>
-                            <div class="button_like">
-                                <i class="bi bi-heart"></i>
-                            </div>
-                        </div>
-
-                        <button class="button_cart">Lägg i varukorg</button>
-                    </div>
-                </li>
-            </ul>
-            <div class="div_button_more_products">
-                <button class="button_more_products">Mer Grönsaker <i class="bi bi-arrow-right"></i></button>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
             </div>
-        </section>
 
-        <section>
-            <h2>I säsong</h2>
-            <ul>
-                <li>
-                    <div class="specific_product">
-                        <div class="img-wrapper">
-                            <img src="../assets/media/Artichoke.jpg" alt="">
-                        </div>
-                        <div class="h3_and_heart">
-                            <h3>Äpple Alice</h3>
-                            <div class="button_like">
-                                <i class="bi bi-heart"></i>
-                            </div>
-                        </div>
-
-                        <button class="button_cart">Lägg i varukorg</button>
-
-
-                    </div>
-                </li>
-                <li>
-                    <div class="specific_product">
-                        <div class="img-wrapper">
-                            <img src="../assets/media/Artichoke.jpg" alt="">
-                        </div>
-                        <div class="h3_and_heart">
-                            <h3>Äpple Alice</h3>
-                            <div class="button_like">
-                                <i class="bi bi-heart"></i>
-                            </div>
-                        </div>
-
-                        <button class="button_cart">Lägg i varukorg</button>
-
-
-                    </div>
-                </li>
-                <li>
-                    <div class="specific_product">
-                        <div class="img-wrapper">
-                            <img src="../assets/media/Artichoke.jpg" alt="">
-                        </div>
-                        <div class="h3_and_heart">
-                            <h3>Äpple Alice</h3>
-                            <div class="button_like">
-                                <i class="bi bi-heart"></i>
-                            </div>
-                        </div>
-
-                        <button class="button_cart">Lägg i varukorg</button>
-
-
-                    </div>
-                </li>
-                <li>
-                    <div class="specific_product">
-                        <div class="img-wrapper">
-                            <img src="../assets/media/Artichoke.jpg" alt="">
-                        </div>
-                        <div class="h3_and_heart">
-                            <h3>Äpple Alice</h3>
-                            <div class="button_like">
-                                <i class="bi bi-heart"></i>
-                            </div>
-                        </div>
-
-                        <button class="button_cart">Lägg i varukorg</button>
-                    </div>
-                </li>
-            </ul>
-            <div class="div_button_more_products">
-                <button class="button_more_products">Mer i Säsong <i class="bi bi-arrow-right"></i></button>
+            <button class="button_cart">Lägg i varukorg</button>
+          </div>
+        </li>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Alice_Apple.jpg" alt="">
             </div>
-        </section>
-    </article>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+
+
+          </div>
+        </li>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Alice_Apple.jpg" alt="">
+            </div>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+
+
+          </div>
+        </li>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Alice_Apple.jpg" alt="">
+            </div>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+
+
+          </div>
+        </li>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Alice_Apple.jpg" alt="">
+            </div>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+
+
+          </div>
+        </li>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Alice_Apple.jpg" alt="">
+            </div>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+          </div>
+        </li>
+      </ul>
+      <div class="div_button_more_products">
+        <button class="button_more_products">Mer Frukter <i class="bi bi-arrow-right"></i></button>
+      </div>
+    </section>
+
+    <section>
+      <h2>Grönsaker</h2>
+      <ul>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Carrot.jpg" alt="">
+            </div>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+
+
+          </div>
+        </li>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Carrot.jpg" alt="">
+            </div>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+
+
+          </div>
+        </li>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Carrot.jpg" alt="">
+            </div>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+
+
+          </div>
+        </li>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Carrot.jpg" alt="">
+            </div>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+          </div>
+        </li>
+      </ul>
+      <div class="div_button_more_products">
+        <button class="button_more_products">Mer Grönsaker <i class="bi bi-arrow-right"></i></button>
+      </div>
+    </section>
+
+    <section>
+      <h2>I säsong</h2>
+      <ul>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Artichoke.jpg" alt="">
+            </div>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+
+
+          </div>
+        </li>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Artichoke.jpg" alt="">
+            </div>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+
+
+          </div>
+        </li>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Artichoke.jpg" alt="">
+            </div>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+
+
+          </div>
+        </li>
+        <li>
+          <div class="specific_product">
+            <div class="img-wrapper">
+              <img src="../assets/media/Artichoke.jpg" alt="">
+            </div>
+            <div class="h3_and_heart">
+              <h3>Äpple Alice</h3>
+              <div class="button_like">
+                <i class="bi bi-heart"></i>
+              </div>
+            </div>
+
+            <button class="button_cart">Lägg i varukorg</button>
+          </div>
+        </li>
+      </ul>
+      <div class="div_button_more_products">
+        <button class="button_more_products">Mer i Säsong <i class="bi bi-arrow-right"></i></button>
+      </div>
+    </section>
+  </article>
 </template>
 
 <style scoped>
 section {
-    margin: 30px 0px;
+  margin: 30px 0px;
 }
 
 ul {
-    display: grid;
-    grid-template-columns: auto auto;
-    grid-template-rows: auto auto;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 1rem;
 }
 
 li {
-    margin: 10px;
+  max-width: 10.4375rem;
 }
 
 .specific_product {
-    display: flex;
-    flex-direction: column;
-    border-radius: 19px;
-    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
-    background-color: #DFD3C3;
+  display: flex;
+  flex-direction: column;
+  border-radius: 19px;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  background-color: #DFD3C3;
 }
 
 .img-wrapper {
-    width: 290px;
-    height: 221px;
+  width: 10.4375rem;
+  height: 10.4375rem;
 }
 
 .img-wrapper img {
-    width: 100%;
-    height: 100%;
-    border-top-right-radius: 19px;
-    border-top-left-radius: 19px;
-    border-bottom-left-radius: 0px;
-    border-bottom-right-radius: 0px;
-    object-fit: cover;
-    object-position: center;
+  width: 100%;
+  height: 100%;
+  border-top-right-radius: 19px;
+  border-top-left-radius: 19px;
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 0px;
+  object-fit: cover;
+  object-position: center;
 }
 
 .h3_and_heart {
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 
 h3 {
-    color: black;
-    font-weight: lighter;
-    /* justify-content: left;
+  color: black;
+  font-weight: lighter;
+  /* justify-content: left;
     align-items: left; */
 }
 
@@ -285,14 +319,14 @@ h3 {
 h3,
 .button_like,
 .button_cart {
-    margin: 7px;
+  margin: 7px;
 }
 
 
 
 /* CSS for buttons in general */
 button:hover {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 /* Knapp för varukorg */
@@ -300,42 +334,53 @@ button:hover {
 
 .button_cart {
 
-    clear: both;
-    margin-left: auto;
-    margin-right: 2;
-    background-color: #FFF8EE;
-    border: 0;
-    text-align: center;
-    border-radius: 19px;
-    padding: 0.6rem;
-    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
-    padding: 5px 10px;
+  clear: both;
+  margin-left: auto;
+  margin-right: 2;
+  background-color: #FFF8EE;
+  border: 0;
+  text-align: center;
+  border-radius: 19px;
+  padding: 0.6rem;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  padding: 5px 10px;
 
 }
 
 .div_button_more_products {
-    clear: both;
-    display: flex;
-    justify-content: flex-end;
+  clear: both;
+  display: flex;
+  justify-content: flex-end;
 
 }
 
 .button_more_products {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 10px;
-    font-size: 20px;
-    border: 0;
-    text-align: center;
-    border-radius: 50px;
-    padding: 0.6rem;
-    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
-    background-color: var(--dusty-pink);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  font-size: 20px;
+  border: 0;
+  text-align: center;
+  border-radius: 50px;
+  padding: 0.6rem;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  background-color: var(--dusty-pink);
 }
 
 .button_more_products i {
-    font-size: 30px;
-    padding-left: 15px;
+  font-size: 30px;
+  padding-left: 15px;
+}
+
+@media screen and (max-width: 385px) {
+  li {
+    max-width: 9.5rem;
+  }
+
+  .img-wrapper {
+    width: 9.5rem;
+    height: 9.5rem;
+  }
 }
 </style>
