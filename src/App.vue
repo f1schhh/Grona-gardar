048d57d5af43abd55b1f010322ee8636c52a0754
Adam Fischer <38475224+f1schhh@users.noreply.github.com>
2024-02-16 14:00:53 +0100

    Uppdatering

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script>
 import Header from './components/Header.vue';
-<<<<<<< HEAD
 import Footer from './components/Footer.vue'
 import SearchBar from './components/SearchBar.vue';
 
-=======
-
-
->>>>>>> fb9162617b38f4d93358dba5a97aae13e81668a0
 
 
 export default {
@@ -19,11 +14,8 @@ export default {
   },
   components: {
     Header,
-<<<<<<< HEAD
     Footer,
     SearchBar
-=======
->>>>>>> fb9162617b38f4d93358dba5a97aae13e81668a0
   },
 }
 
