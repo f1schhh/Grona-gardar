c867e950684510948cbb50bd423d828df97b4224
TryFrySpy <mela.ingrid.cecilia@gmail.com>
2024-02-20 17:24:23 +0100

    Redigerat AccountView.vue

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,53 +1,53 @@
 <script>
 export default {
-  data() {
-    return {
-      searchQuery: null,
+    data() {
+        return {
+            searchQuery: null,
+        }
+    },
+    methods: {
+        searchClick() {
+            if (this.searchQuery) {
+                this.$router.push('/search/' + this.searchQuery);
+            }
+        }
     }
-  },
-  methods: {
-    searchClick() {
-      if (this.searchQuery) {
-        this.$router.push('/search/' + this.searchQuery);
-      }
-    }
-  }
 }
 </script>
 
 <style scoped>
 #searchDiv {
-  width: 80%;
-  display: flex;
-  align-items: center;
-  position: relative;
+    width: 80%;
+    display: flex;
+    align-items: center;
+    position: relative;
 }
 
 
 #searchInput {
-  border-radius: 2rem;
-  height: 1.5rem;
-  background-color: var(--light-beige);
-  border: inset var(--dark-green) 1.5px;
-  box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.3) inset;
-  text-align: left;
-  color: var(--dark-green);
-  width: 100%;
-  padding: 0.25rem;
-  padding-left: 1rem;
+    border-radius: 2rem;
+    height: 1.5rem;
+    background-color: var(--light-beige);
+    border: inset var(--dark-green) 1.5px;
+    box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.3) inset;
+    text-align: left;
+    color: var(--dark-green);
+    width: 100%;
+    padding: 0.25rem;
+    padding-left: 1rem;
 }
 
 
 #searchIcon {
-  position: absolute;
-  right: 10px;
+    position: absolute;
+    right: 10px;
 }
 </style>
 
 <template>
-  <div id="searchDiv">
-    <input type="text" id="searchInput" v-model="searchQuery" @keyup.enter="searchClick"
-      placeholder="Sök i vårt sortiment..." required>
-    <i id="searchIcon" class="bi bi-search" @click="searchClick"></i>
-  </div>
+    <div id="searchDiv">
+        <input type="text" id="searchInput" v-model="searchQuery" @keyup.enter="searchClick"
+            placeholder="Sök i vårt sortiment..." required>
+        <i id="searchIcon" class="bi bi-search" @click="searchClick"></i>
+    </div>
 </template>
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,8 +1,264 @@
 <script></script>
 
-<style setup></style>
+<style setup>
+main {
+    display: flex;
+    flex-direction: column;
+    /* flex-wrap: wrap; */
+    row-gap: 0.7rem;
+    background-color: var(--dark-beige);
+    text-align: center;
+}
+
+#profileInfo {
+    background-color: var(--mid-beige);
+}
+
+#orderHistory {
+    background-color: pink;
+
+}
+
+#orderHistory h2 {
+    font-weight: bold;
+    font-size: 1rem;
+}
+
+#profilePic {
+    width: 4rem;
+    border-radius: 100%;
+}
+
+h2 {
+    font-weight: lighter;
+    font-size: 1.2rem;
+
+}
+
+ul {
+    display: flex;
+    justify-content: center;
+    /* align-items: center; */
+}
+
+.profileInfo_li {
+    display: flex;
+    flex-direction: column;
+    font-size: 0.8rem;
+    /* flex-wrap: wrap; */
+    padding: 5px;
+    row-gap: 0.5rem;
+}
+
+i {
+    font-size: 1.2rem;
+}
+
+.hrAside {
+    height: 2.5rem;
+}
+
+#endAside {
+    border: 1px solid #475133;
+}
+
+/* .product_list li {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+} */
+
+.img-wrapper {
+    width: 10.4375rem;
+    height: 10.4375rem;
+}
+
+.img-wrapper img {
+    width: 100%;
+    height: 100%;
+    border-top-right-radius: 0rem;
+    border-top-left-radius: 1rem;
+    border-bottom-left-radius: 1rem;
+    border-bottom-right-radius: 0rem;
+    object-fit: cover;
+    object-position: center;
+}
+
+
+/*CSS for buttons in general */
+button:hover {
+    cursor: pointer;
+}
+
+/* Knapp för varukorg */
+
+button_seeOrder_div {
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+}
+
+.button_seeOrder {
+
+    /* clear: both; */
+    /* margin-left: auto;
+    margin-right: 2; */
+    background-color: #FFF8EE;
+    border: 0;
+    text-align: center;
+    border-radius: 19px;
+    padding: 0.6rem;
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+    padding: 5px 10px;
+
+}
+
+.orderHistory_list {
+    display: flex;
+    flex-direction: column;
+    /* flex-wrap: wrap; */
+    gap: 1rem;
+    /* justify-content: center; */
+    align-items: center;
+    color: black;
+}
+
+.orderHistory_list_li {
+    display: flex;
+    text-align: left;
+    background-color: var(--dark-beige);
+    /* flex-wrap: wrap; */
+    flex-direction: row;
+    /* padding: 0rem 5rem; */
+    border-top-right-radius: 1rem;
+    border-top-left-radius: 1rem;
+    border-bottom-left-radius: 1rem;
+    border-bottom-right-radius: 1rem;
+    padding: 0rem 2.7rem;
+}
+
+@media screen and (max-width: 385px) {
+    main {
+        width: 100%;
+    }
+
+    li {
+        max-width: 20rem;
+    }
+
+    .img-wrapper {
+        width: 6.5rem;
+        height: 6.5rem;
+    }
+}
+</style>
 
 
 <template>
-    <div>This is your personal account...</div>
+    <main>
+        <aside id="profileInfo">
+            <h1>Min Profil</h1><img id="profilePic" src="../assets/media/profile_pictures/profilePic1.jpg" alt="">
+            <h2>Namn</h2>
+            <ul>
+                <li class="profileInfo_li">
+                    <i class="bi bi-basket3"></i>
+                    <p>Orderhistorik</p>
+                </li>
+
+                <li class="profileInfo_li">
+                    <hr class="hrAside">
+                </li>
+                <li class="profileInfo_li"><i class="bi bi-person"></i>
+                    <p>Mitt konto</p>
+                </li>
+                <li class="profileInfo_li">
+                    <hr class="hrAside">
+                </li>
+                <li class="profileInfo_li">
+                    <i class="bi bi-heart"></i>
+                    <p>
+                        Favoriter
+                    </p>
+                </li>
+                <li class="profileInfo_li">
+                    <hr class="hrAside">
+                </li>
+                <li class="profileInfo_li">
+                    <i class="bi bi-box-arrow-left"></i>
+                    <p>Logga ut</p>
+                </li>
+            </ul>
+            <!-- <hr id="endAside"> -->
+        </aside>
+
+        <!-- start på section -->
+        <!-- start på section -->
+        <section id="orderHistory">
+            <h2>Orderhistorik</h2>
+            <hr id="endAside">
+            <ul class="orderHistory_list">
+                <li class="orderHistory_list_li">
+                    <div class="img-wrapper">
+                        <img src="../assets/media/product_images/Pumpkin_Howden.jpg" alt="">
+                    </div>
+                    <div>
+                        <div class="order_summary">
+                            <h3>Ordernummer:</h3>
+                        </div>
+                        <div class="button_seeOrder_div">
+                            <button class="button_seeOrder">Se order</button>
+                        </div>
+                    </div>
+                </li>
+                <li class="orderHistory_list_li">
+                    <div class="img-wrapper">
+                        <img src="../assets/media/product_images/Pumpkin_Howden.jpg" alt="">
+                    </div>
+                    <div>
+                        <div class="order_summary">
+                            <h3>Ordernummer:</h3>
+                        </div>
+
+                        <button class="button_seeOrder">Se order</button>
+                    </div>
+                </li>
+                <li class="orderHistory_list_li">
+                    <div class="img-wrapper">
+                        <img src="../assets/media/product_images/Pumpkin_Howden.jpg" alt="">
+                    </div>
+                    <div>
+                        <div class="order_summary">
+                            <h3>Ordernummer:</h3>
+                        </div>
+
+                        <button class="button_seeOrder">Se order</button>
+                    </div>
+                </li>
+                <li class="orderHistory_list_li">
+                    <div class="img-wrapper">
+                        <img src="../assets/media/product_images/Pumpkin_Howden.jpg" alt="">
+                    </div>
+                    <div>
+                        <div class="order_summary">
+                            <h3>Ordernummer:</h3>
+                        </div>
+
+                        <button class="button_seeOrder">Se order</button>
+                    </div>
+                </li>
+                <li class="orderHistory_list_li">
+                    <div class="img-wrapper">
+                        <img src="../assets/media/product_images/Pumpkin_Howden.jpg" alt="">
+                    </div>
+                    <div>
+                        <div class="order_summary">
+                            <h3>Ordernummer:</h3>
+                        </div>
+
+                        <button class="button_seeOrder">Se order</button>
+                    </div>
+                </li>
+            </ul>
+        </section>
+    </main>
 </template>
