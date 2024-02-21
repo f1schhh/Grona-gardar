84ed3aad741c86d50ec333dfa0dcac2a584268df
AtQ0 <mail@atkobabic.com>
2024-02-21 15:58:49 +0100

    working on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,41 @@
 <template>
-
     <footer>
         <div class="svg-container">
-            <svg id="Lager_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 519.54 50.15"><defs></defs><path class="cls-1" d="M519.54,50.15H0v-30.13s72.54,26.65,149.74,26.75c48.5.06,87-7.98,111.49-15.03,21.91-6.31,81.6-29.73,135.69-29.73,62.44,0,122.62,26.33,122.62,26.33v21.81Z"/></svg>
+            <svg id="Lager_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 519.54 50.15">
+                <defs></defs>
+                <path class="cls-1"
+                    d="M519.54,50.15H0v-30.13s72.54,26.65,149.74,26.75c48.5.06,87-7.98,111.49-15.03,21.91-6.31,81.6-29.73,135.69-29.73,62.44,0,122.62,26.33,122.62,26.33v21.81Z" />
+            </svg>
         </div>
         <div class="footer-text-wrapper">
-        <h2>Om oss</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi, necessitatibus adipisci sunt sapiente magnam saepe provident odio nulla hic, aspernatur, qui recusandae? Autem nemo quidem fugit aliquid quae iste officiis!</p>
 
-        <h2>Kontakt</h2>
-        <p>Har du frågor eller vill komma i kontakt med oss? Tveka inte att nå ut till vår kundtjänst.</p>
-    </div>
+            <div class="about-us-container">
+                <h2>Om oss</h2>
+                <p>Gröna gårdar startades 1991 av Glenn Olsson och har sedan dess levererat närodlat och ekologiskt till
+                    kunder, både nära och från långa vägar.</p>
+                <p><b>Detta är en fiktiv sida designad av:</b> Bahareh, Emil, Kajsa och Ramin.</p>
+                <p><b>Och framtagen av:</b> Adam, Anna, Atko, Kajsa och Mela.</p>
+            </div>
+
+            <div class="visit-us-container">
+                <h2>Besök oss</h2>
+                <p>Vår gårdsbutik är lätt att hitta. Besök oss personligen och upplev det bästa av lokala råvaror.</p>
+
+                <h4>Adress:</h4>
+                <p>Grönagårdar 12, 400 23 Lindome</p>
+
+                <h4>Öppettider:</h4>
+                <p>Måndag - torsdag: 11:00 - 18:00</p>
+                <p>Lördag - Söndag: 10:00-16:00</p>
+            </div>
+
+            <div class="contact-us-container">
+                <h2>Kontakt</h2>
+                <p>Har du frågor eller vill komma i kontakt med oss? Tveka inte att nå ut till vår kundtjänst.</p>
+                <p><b>Kundtjänst:</b> info@grönagårdar.com</p>
+                <p><b>Telefon:</b> 032-234234</p>
+            </div>
+        </div>
         <!-- <div class="shapedividers_com-2880"></div> -->
     </footer>
 </template>
@@ -18,31 +43,36 @@
 
 
 <style scoped>
-
-footer{
+footer {
     width: 100vw;
+    text-align: center;
+    background-color: var(--light-beige);
 }
-.footer-text-wrapper{
+
+.footer-text-wrapper {
+    padding-left: 25px;
+    padding-right: 25px;
     background-color: var(--dark-green);
     color: var(--light-beige);
     width: 100vw;
     height: 100%;
 }
-.svg-container{
+
+.svg-container {
     width: 100%;
     background-color: transparent;
 }
-.svg-container svg{
+
+.svg-container svg {
     width: 100%;
     height: 100%;
     margin-bottom: 0;
     margin-bottom: -5px;
 }
 
-.cls-1{
+.cls-1 {
     fill: var(--dark-green);
-    stroke-width:0px;
+    stroke-width: 0px;
     bottom: 0;
-    }
-
+}
 </style>
