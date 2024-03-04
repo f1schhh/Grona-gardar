6ea644ee5a6785c430c6a596ff4fd9b72ea78869
Anna Bäckegren <anna.backegren@gmail.com>
2024-03-04 10:46:51 +0100

    funktion för favoritknapp klar

---
+++ src/components/FavoriteButton.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+    props: {
+        id: {
+            type: Number,
+        }
+    },
+    data() {
+        return {
+            heartClass: 'bi bi-heart',
+        };
+    },
+
+    created(){
+        this.loadClass();
+    },
+
+    methods: {
+        loadClass(){
+            const currentFavorite = localStorage.getItem('favorite')
+            const checkFavorite = currentFavorite ? JSON.parse(currentFavorite) : [];
+
+            if (checkFavorite.includes(this.id)){
+                this.heartClass = 'bi bi-heart-fill';
+            }
+        },
+
+        favoriteClick() {
+            const currentFavorite = localStorage.getItem('favorite')
+            const checkFavorite = currentFavorite ? JSON.parse(currentFavorite) : [];
+
+            if (!checkFavorite.includes(this.id) || checkFavorite === null){
+                checkFavorite.push(this.id)
+                localStorage.setItem('favorite', JSON.stringify(checkFavorite))
+                this.heartClass = 'bi bi-heart-fill';
+                localStorage.setItem('heartClass_${this.id}', this.heartClass);
+                console.log(checkFavorite)
+            } else {
+                const deleteFavorite = checkFavorite.filter(item => item !== this.id)
+                localStorage.setItem('favorite', JSON.stringify(deleteFavorite))
+                this.heartClass = 'bi bi-heart';
+                localStorage.setItem('heartClass_${this.id}', this.heartClass);
+                console.log(checkFavorite)
+            }
+        },
+    },
+
+};
+</script>
+
+<template>
+    <div class="button_like" >
+        <i @click="favoriteClick" :class="heartClass"></i>
+    </div>
+</template>
